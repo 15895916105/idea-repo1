b3274642bd616fc0e3b7973977c83227a938b3a5
chenzhen <1065228646@qq.com>
2020-11-01 22:54:51 +0800

    删除无用文件

--- git-test-project1/src/TestDemo1.java
+++ git-test-project1/src/TestDemo1.java
@@ -4,5 +4,6 @@ public class TestDemo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("11");
+        System.out.println();
     }
 }
